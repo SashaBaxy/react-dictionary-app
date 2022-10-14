fc20682258f03324f2fcf9df409a5a4ecdb19f86
SashaBaxy <101371820+SashaBaxy@users.noreply.github.com>
2022-10-14 21:59:32 +0100

    added dictionary component and updated search function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import book from "./book.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={book} className="App-logo" alt="book" />
-            </header>
+            <div className="container">
+                <header className="App-header">
+                    <img src={book} className="App-logo" alt="book" />
+                </header>
+                <main>
+                    <Dictionary />
+                </main>
+                <footer className="App-footer">
+                    <small>Coded by Sasha Baxendale</small>
+                </footer>
+            </div>
         </div>
     );
 }
