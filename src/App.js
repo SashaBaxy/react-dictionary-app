263fe6bbf53a0a8a695e7db50768d57fdd921cdd
SashaBaxy <101371820+SashaBaxy@users.noreply.github.com>
2022-10-14 20:09:48 +0100

    Added logo to app.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import book from "./book.png";
+import "./App.css";
+
+export default function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={book} className="App-logo" alt="book" />
+            </header>
+        </div>
+    );
+}
